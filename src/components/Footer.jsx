48ba2df1218d8766fd16ee24cea4e85f4dec8c00
zealfemi <zealfemi@gmail.com>
2026-04-04 00:21:29 +0100

    updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,9 @@ export default function Footer() {
   return (
     <footer>
       <span>Ⓒ 2026 #VANLIFE</span>
-      <span>Gbackie</span>
+      <span>
+        <a href="https://zealfemi.vercel.app">Gbackie</a>
+      </span>
     </footer>
   );
 }
