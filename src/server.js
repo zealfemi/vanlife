3eb4082bf3f50fd517e4b074c7cc4ed5738f706d
zealfemi <zealfemi@gmail.com>
2026-04-04 01:33:58 +0100

    updated

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ createServer({
       price: 60,
       description:
         "The Modest Explorer is a van designed to get you out of the house and into nature. This beauty is equipped with solar panels, a composting toilet, a water tank and kitchenette. The idea is that you can pack up your home and escape for a weekend or even longer!",
-      imageUrl: "./images/modest-explorer.png",
+      imageUrl: "/images/modest-explorer.png",
       type: "simple",
     });
     server.create("van", {
@@ -21,7 +21,7 @@ createServer({
       price: 80,
       description:
         "Beach Bum is a van inspired by surfers and travelers. It was created to be a portable home away from home, but with some cool features in it you won't find in an ordinary camper.",
-      imageUrl: "./images/beach-bum.png",
+      imageUrl: "/images/beach-bum.png",
       type: "rugged",
     });
     server.create("van", {
@@ -30,7 +30,7 @@ createServer({
       price: 100,
       description:
         "Reliable Red is a van that was made for travelling. The inside is comfortable and cozy, with plenty of space to stretch out in. There's a small kitchen, so you can cook if you need to. You'll feel like home as soon as you step out of it.",
-      imageUrl: "./images/reliable-red.png",
+      imageUrl: "/images/reliable-red.png",
       type: "luxury",
     });
     server.create("van", {
@@ -39,7 +39,7 @@ createServer({
       price: 65,
       description:
         "Dreamfinder is the perfect van to travel in and experience. With a ceiling height of 2.1m, you can stand up in this van and there is great head room. The floor is a beautiful glass-reinforced plastic (GRP) which is easy to clean and very hard wearing. A large rear window and large side windows make it really light inside and keep it well ventilated.",
-      imageUrl: "./images/dream-finder.png",
+      imageUrl: "/images/dream-finder.png",
       type: "simple",
     });
     server.create("van", {
@@ -48,7 +48,7 @@ createServer({
       price: 120,
       description:
         "The Cruiser is a van for those who love to travel in comfort and luxury. With its many windows, spacious interior and ample storage space, the Cruiser offers a beautiful view wherever you go.",
-      imageUrl: "./images/the-cruiser.png",
+      imageUrl: "/images/the-cruiser.png",
       type: "luxury",
     });
     server.create("van", {
@@ -57,7 +57,7 @@ createServer({
       price: 70,
       description:
         "With this van, you can take your travel life to the next level. The Green Wonder is a sustainable vehicle that's perfect for people who are looking for a stylish, eco-friendly mode of transport that can go anywhere.",
-      imageUrl: "./images/green-wonder.png",
+      imageUrl: "/images/green-wonder.png",
       type: "rugged",
     });
   },
