99f529c25f5f48d86702dd09d65312a149afd62d
zealfemi <zealfemi@gmail.com>
2026-04-13 13:00:06 +0100

    updated

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,11 @@
 import { useState } from "react";
-import { Link, useLoaderData, Form, redirect } from "react-router-dom";
+import {
+  Link,
+  useLoaderData,
+  useActionData,
+  Form,
+  redirect,
+} from "react-router-dom";
 
 import { loginUser } from "../api";
 
@@ -10,6 +16,7 @@ export function Loader({ request }) {
     response.body = true;
     return response;
   }
+
   // IF NOT LOGGED IN
   const url = new URL(request.url).searchParams.get("message");
   return url;
@@ -20,49 +27,32 @@ export async function Action({ request }) {
   const email = formData.get("email");
   const password = formData.get("password");
 
-  const data = await loginUser({ email, password });
-  localStorage.setItem("loggedIn", true);
-  const res = redirect("/host");
-  res.body = true;
+  try {
+    const data = await loginUser({ email, password });
+    localStorage.setItem("loggedIn", true);
 
-  return res;
+    const res = redirect("/host");
+    res.body = true;
+    return res;
+  } catch (err) {
+    return err.message;
+  }
+  // return null;
 }
 
 export default function Login() {
-  const [status, setStatus] = useState("idle");
-  const [error, setError] = useState(null);
-  const [loggedIn, setLoggedIn] = useState(false);
-
   const loginMessage = useLoaderData();
-  // const actionData = useActionData();
-  // console.log(actionData);
-
-  // const navigate = useNavigate();
-
-  // function handleSubmit(e) {
-  //   e.preventDefault();
-  //   // console.log(loginFormData);
-  //   // loginUser(loginFormData).then((data) => console.log(data));
-
-  //   setStatus("submitting");
-  //   setError(null);
-  //   loginUser(loginFormData)
-  //     .then((data) => {
-  //       navigate("/host", { replace: true });
-  //     })
-  //     .catch((error) => {
-  //       setError(error.message);
-  //     })
-  //     .finally(() => setStatus("idle"));
-  // }
+  const errorMessage = useActionData();
 
   return (
     <div className="login-container">
       <h1>Sign in to your account</h1>
 
       <div>
-        {loginMessage && <h3 style={{ color: "red" }}>{loginMessage}</h3>}
-        {error && <h3 style={{ color: "red" }}>{error}</h3>}
+        {loginMessage && !errorMessage && (
+          <h3 style={{ color: "red" }}>{loginMessage}</h3>
+        )}
+        {errorMessage && <h3 style={{ color: "red" }}>{errorMessage}</h3>}
       </div>
 
       <Form method="post" className="login-form" replace>
